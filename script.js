966596f88d03ca2459b519ae934f001763d76838
videju <77975858+videju@users.noreply.github.com>
2023-03-19 12:26:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -81,18 +81,11 @@ switchMode.addEventListener('change', function () {
   //login_section code
 
   const loginForm = document.querySelector("form.login");
-  const signupForm = document.querySelector("form.signup");
+
   const loginBtn = document.querySelector("label.login");
-  const signupBtn = document.querySelector("label.signup");
-  const signupLink = document.querySelector(".signup-link a");
-  
-  signupBtn.onclick = (()=>{
-	loginForm.style.marginLeft = "-50%"
-  })
-  loginBtn.onclick = (()=>{
-	loginForm.style.marginLeft = "0%"
-  })
- 
+
+    
+
   
  
   //
